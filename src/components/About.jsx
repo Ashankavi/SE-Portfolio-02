e02efaddb6b16b3c5a28738b5a275ca46f4b580a
Ashan Kavindu <134072313+Ashankavi@users.noreply.github.com>
2024-10-30 13:52:17 +0530

    Project Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,8 @@ import { CardEffectMain } from '../components/Main/CardEffectMain';
 
 const About = () => {
   return (
-    <div className="flex w-full gap-8 p-10 mx-auto bg-[#101010]">
+    <div className="flex flex-col sm:flex-row w-full gap-8 p-4 sm:p-10 mx-auto bg-[#101010]">
+      {/* Section 01 */}
       <div className="flex flex-wrap justify-center flex-1">
         <img
           src={AboutAvatar}
@@ -12,13 +13,11 @@ const About = () => {
           alt="About"
         />
       </div>
-
-      <div className="relative flex-1 p-8 rounded-[50px] overflow-hidden border border-[#666666] flex-wrap">
-        {/* CardEffectMain Background */}
+      {/* Section 02 */}
+      <div className="relative flex-1 p-4 sm:p-8 rounded-[50px] overflow-hidden border border-[#666666] flex-wrap">
         <CardEffectMain className="absolute inset-0 z-0 w-full h-full" />
 
-        {/* Text Content */}
-        <div className="relative z-10">
+        <div className="relative z-10 p-4">
           <h2 className="font-sans text-2xl font-bold text-[#2ADCFC]">ABOUT ME</h2>
           <h1 className="font-sans text-4xl font-bold text-[#ffffff] my-8">
             World leading Creative Front-End Developer
@@ -33,7 +32,7 @@ const About = () => {
             and dedicated to work.
           </p>
 
-          <button className="py-4 px-8 rounded-[6px] bg-[#26a8be] text-[#ffffff] my-8 uppercase">Download CV</button>
+          <button className="py-4 px-8 rounded-[6px] bg-[#26a8be] text-[#ffffff] mt-4 uppercase">Download CV</button>
         </div>
       </div>
     </div>
